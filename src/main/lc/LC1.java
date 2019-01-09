778e1b4455b5c10d708c88943580f3f78ca3350e
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-01-09 22:11:05 +0100

    HR daily practice

---
+++ src/main/lc/LC1.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * https://leetcode.com/problems/two-sum/
+ * Difficulty: easy
+ *
+ */
+
+import java.util.*;
+
+class LC1 {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            ArrayList<Integer> list = map.getOrDefault(nums[i], new ArrayList<>());
+            list.add(i);
+            map.put(nums[i], list);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            int x = nums[j];
+            int y = target - x;
+            if (map.containsKey(y) && map.get(y) != null && map.get(y).size() > 0) {
+                int idx0 = map.get(x).get(0);
+                ArrayList<Integer> ySet = map.get(y);
+                if (ySet != null && ySet.size() > 0) {
+                    for (int k = 0; k < ySet.size(); k++) {
+                        if (ySet.get(k) != idx0) {
+                            int[] res = new int[]{idx0, ySet.get(k)};
+                            map.get(y).remove(0);
+                            map.get(x).remove(0);
+                            return res;
+                        }
+                    }
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] res = LC1.twoSum(new int[] {3,2,4}, 6);
+        System.out.println(Arrays.toString(res));
+    }
+}
---
+++ src/main/lc/LC4.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ * Difficulty: hard
+ */
+
+import java.util.Arrays;
+
+public class LC4 {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, merged, 0, nums1.length);
+        System.arraycopy(nums2, 0, merged, nums1.length, nums2.length);
+
+        Arrays.sort(merged);
+        double mediana;
+        if (merged.length % 2 == 0) {
+            mediana = ((double)merged[merged.length/2] + merged[merged.length/2 - 1])/2;
+        } else {
+            mediana = merged[merged.length/2];
+        }
+        return mediana;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(LC4.findMedianSortedArrays(
+                new int[] {1,2,3}, new int[] {3,4}
+        ));
+    }
+}
