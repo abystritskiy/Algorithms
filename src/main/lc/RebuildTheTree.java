4e95ed46d456192a6b5bf3530927add94140f6d9
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-01-22 00:12:26 +0100

    Daily practice & pramp

---
+++ src/main/lc/RebuildTheTree.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/discuss/interview-question/192492/Amazon-phone-screen-Rebuild-binary-tree-given-an-input-matrix-of-nodes.
+ */
+public class RebuildTheTree {
+
+
+    private class Node {
+        private final int val;
+        Node left = null;
+        Node right = null;
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
