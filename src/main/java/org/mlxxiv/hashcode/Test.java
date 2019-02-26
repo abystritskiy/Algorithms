0de1b98a4c82ddc25ffeb5efcb1e1b282771d9ee
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-02-26 23:05:30 +0100

    Daily practice

--- src/main/java/org/mlxxiv/hashcode/Test.java
+++ src/main/java/org/mlxxiv/hashcode/Test.java
@@ -7,7 +7,9 @@ public class Test {
     public static void main(String[] args) {
         Input in = new Input("input/hashcode/pizza/big.in");
         Pizza pizza = new Pizza(in.grid);
-        for (int[] coord: in.readResultsFile()) {
+        int[][] coords = in.readResultsFile();
+        for (int i =0; i<coords.length; i++) {
+            int[] coord = coords[i];
             int tomatoes = 0;
             int area = (coord[2] - coord[0] + 1) * (coord[3] - coord[1] + 1);
             for (int y = coord[0]; y <=coord[2] ; y++) {
@@ -18,7 +20,7 @@ public class Test {
                 }
             }
             if (tomatoes<6 || area - tomatoes < 6) {
-                System.out.println("fail!!! " + Arrays.toString(coord));
+                System.out.println(i+ ": fail!!! " + Arrays.toString(coord));
             }
         }
     }
