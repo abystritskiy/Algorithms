4e1cedc3cbce384571d81e506c7eac18b4a6e3bb
abystritskiy <alex.bystritskiy@gmail.com>
2019-06-28 18:13:12 +0300

    FB3_1

--- src/main/java/org/mlxxiv/hr/FB3_1.java
+++ src/main/java/org/mlxxiv/hr/FB3_1.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class FB3_1 {
@@ -5,26 +6,52 @@ public class FB3_1 {
         System.out.println(solution("9", "13")); // 6
         System.out.println(solution("21", "29")); // 7
         System.out.println(solution("1", "2")); //1
-        System.out.println(solution("4", "7")); //4
+        System.out.println(solution("4", "31")); //10
         System.out.println(solution("1", "1")); //0
         System.out.println(solution("11", "4")); //5
+        System.out.println(solution("425", "277")); //16
+        System.out.println(solution("2", "4")); //impossible
+        System.out.println(solution("1", "3")); //2
+        System.out.println(solution("1", "10000000000000000001")); //2
     }
 
     public static String solution(String x, String y) {
-        int m = Integer.parseInt(x);
-        int f = Integer.parseInt(y);
-        int i = 0;
+        BigInteger m = new BigInteger(x);
+        BigInteger f = new BigInteger(y);
+        BigInteger i = new BigInteger("0");
 
-        while (m >= 1 && f >= 1) {
-            if (m == 1 && f == 1) {
-                return Integer.toString(i);
+        BigInteger two = new BigInteger("2");
+        BigInteger one = new BigInteger("1");
+        BigInteger zero = new BigInteger("0");
+
+
+        if (f.mod(two).equals(zero) &&
+                m.mod(two).equals(zero)) {
+            return "impossible";
+        }
+
+        while ((m.compareTo(one) > 0 || m.compareTo(one) == 0)
+                && (f.compareTo(one) > 0 || f.compareTo(one) == 0)
+        ) {
+            if (m.equals(one) && f.equals(one)) {
+                return i.toString();
+            } else if ((f.equals(one) && m.equals(two)) ||
+                    (m.equals(one) && f.equals(two))) {
+                i = i.add(one);
+                return i.toString();
             } else {
-                if (f > m) {
-                    f -= m;
+                if (f.compareTo(m) > 0) {
+                    BigInteger cycles = f.mod(m).equals(zero)  ?
+                            f.divide(m).subtract(one) : f.divide(m);
+                    i = i.add(cycles);
+                    f = f.subtract(m.multiply(cycles));
                 } else {
-                    m -= f;
+                    BigInteger cycles = m.mod(f).equals(zero) ?
+                            m.divide(f).subtract(one) : m.divide(f);
+                    i = i.add(cycles);
+                    m = m.subtract(f.multiply(cycles));
                 }
-                i++;
+
             }
         }
 
@@ -46,12 +73,13 @@ public class FB3_1 {
         int solution = Integer.MAX_VALUE;
 
         while (stack.size() > 0) {
-            int last = stack.size()-1;
+            int last = stack.size() - 1;
             ArrayList<Integer> cur = stack.get(last);
             stack.remove(last);
 
             int m0 = cur.get(0);
-            int f0 = cur.get(1);;
+            int f0 = cur.get(1);
+            ;
             int cycle = cur.get(2);
 
             if (m0 == m1 && f0 == f1) {
@@ -67,13 +95,13 @@ public class FB3_1 {
 
             ArrayList<Integer> case1 = new ArrayList<>();
             case1.add(m0);
-            case1.add(m0+f0);
-            case1.add(cycle+1);
+            case1.add(m0 + f0);
+            case1.add(cycle + 1);
 
             ArrayList<Integer> case2 = new ArrayList<>();
-            case2.add(m0+f0);
+            case2.add(m0 + f0);
             case2.add(f0);
-            case2.add(cycle+1);
+            case2.add(cycle + 1);
 
             stack.add(case1);
             stack.add(case2);
@@ -81,10 +109,9 @@ public class FB3_1 {
         if (solution == Integer.MAX_VALUE) {
             return "impossible";
         } else {
-            return  Integer.toString(solution);
+            return Integer.toString(solution);
         }
     }
-
 
 
     public static String solutionGreedy(String x, String y) {
@@ -110,7 +137,7 @@ public class FB3_1 {
                 m0 += f0;
             } else if (m0 + f0 == f1) {
                 f0 += m0;
-            } else if (m1-m0 > f1-f0) {
+            } else if (m1 - m0 > f1 - f0) {
                 m0 += f0;
             } else {
                 f0 += m0;
@@ -134,8 +161,8 @@ public class FB3_1 {
             }
             i++;
 
-            System.out.println(m +", " + f);
+            System.out.println(m + ", " + f);
         }
-        return new int[] {m, f};
+        return new int[]{m, f};
     }
 }
