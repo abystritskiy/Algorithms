fa21882522a2b9f4bcce790891ee77d45f5ab332
abystritskiy <alex.bystritskiy@gmail.com>
2019-06-30 21:59:41 +0300

    FB3_1

--- src/main/java/org/mlxxiv/hr/FB3_2.java
+++ src/main/java/org/mlxxiv/hr/FB3_2.java
@@ -13,22 +13,19 @@ public class FB3_2 {
         System.out.println(solution(l2)); //3
 
         int[] l3 = new int[] {1, 2, 3, 4, 5, 6, 8, 48};
-        System.out.println(solution(l3)); //5
+        System.out.println(solution(l3)); //16
+
+        int[] l4 = new int[] {2, 3, 4, 6, 9, 12};
+        System.out.println(solution(l4)); //3
     }
 
     public static int solution(int[] l) {
         Arrays.sort(l);
-        int level;
-        if (l[0] != 1) {
-            level = 0;
-        } else {
-            level = 1;
-        }
+        Node head = new Node(0, 0, "");
 
-        Node head = new Node(l[0], level);
         int count = 0;
 
-        for (int i = 1; i<l.length; i++) {
+        for (int i = 0; i<l.length; i++) {
             head.insert(l[i]);
         }
 
@@ -55,12 +52,14 @@ public class FB3_2 {
     {
         final public int value;
         final public int level;
+        final public String path;
         private HashMap<Integer, Node> children = new HashMap<>();
 
 
-        public Node(int value, int level) {
+        public Node(int value, int level, String path) {
             this.value = value;
             this.level = level;
+            this.path = path;
         }
 
         public HashMap<Integer, Node> children()
@@ -74,17 +73,22 @@ public class FB3_2 {
                     candidate % value == 0;
         }
 
+        public String toString()
+        {
+            return this.path;
+        }
+
         public void insert(int value)
         {
             if (this.level == 2 && this.isValid(value) && !this.children.containsKey(value)) {
-                this.children.put(value, new Node(value, level+1));
+                this.children.put(value, new Node(value, level+1, this.path + "/"+value));
             } else if (this.level < 2) {
                 if (this.level == 0 || this.isValid(value)) {
                     for (Map.Entry<Integer, Node> child: children.entrySet()) {
                         child.getValue().insert(value);
                     }
                     if (!this.children.containsKey(value)) {
-                        this.children.put(value, new Node(value, level+1));
+                        this.children.put(value, new Node(value, level+1, this.path + "/"+value));
                     }
                 }
             }
