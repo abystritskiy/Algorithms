5460ebfe9e17d133debdeb189479e9b4964ac82f
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-08-19 23:41:04 -0700

    Daily practice

--- src/main/java/org/mlxxiv/lc/LC139.java
+++ src/main/java/org/mlxxiv/lc/LC139.java
@@ -1,6 +1,7 @@
 package org.mlxxiv.lc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LC139 {
@@ -11,6 +12,13 @@ public class LC139 {
         String s1 = "leetcode";
         boolean res1 = true;
         System.out.println(wordBreak(s1, dict1) == res1);
+
+        List<String> dict4 = new ArrayList<>();
+        dict4.add("bc");
+        dict4.add("cb");
+        String s4 = "ccbb";
+        boolean res4 = false;
+        System.out.println(wordBreak(s4, dict4) == res4);
 
 
         List<String> dict2 = new ArrayList<>();
@@ -33,22 +41,34 @@ public class LC139 {
     }
 
     public static boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreakRecursive(s, wordDict);
+        HashMap<HashMap<String, List<String>>, Boolean> cache = new HashMap<>();
+        return wordBreakRecursive(s, wordDict, cache);
     }
 
-    private static boolean wordBreakRecursive(String s, List<String> wordDict) {
-        if (s.length() == 0) {
+    private static boolean wordBreakRecursive(String s, List<String> wordDict,
+                                              HashMap<HashMap<String, List<String>>, Boolean> cache) {
+        HashMap<String, List<String>> key = new HashMap<>();
+        key.put(s, wordDict);
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+
+        if (s.trim().length() == 0) {
             return true;
         }
+
         for (String word: wordDict) {
             int pos = s.indexOf(word);
             if (pos != -1) {
-                String sCut = s.substring(0, pos) + s.substring(pos+word.length());
-                if (wordBreakRecursive(sCut, wordDict)) {
+                String sCut = s.substring(0, pos) + " " + s.substring(pos+word.length());
+                if (wordBreakRecursive(sCut, wordDict, cache)) {
+
+                    cache.put(key, true);
                     return true;
                 }
             }
         }
+        cache.put(key, false);
         return false;
     }
 }
